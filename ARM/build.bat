5b901003cbcbe99fd055e4b58ab1ce1ea9e493e2
dominik-chat <dominik.sz.chat@gmail.com>
2021-12-01 00:02:58 +0100

    ARM: Add stack pointer switch and context preservation
    Now context is switched by pushing all regs to stack
    and swaping stack

--- ARM/build.bat
+++ ARM/build.bat
@@ -1,6 +1,7 @@
 arm-none-eabi-gcc -Wall -O0 -mcpu=arm7tdmi-s -marm -c Startup.s -o Startup.o
 arm-none-eabi-gcc -Wall -O0 -mcpu=arm7tdmi-s -marm -c lpc21xx.c -o lpc21xx.o
-arm-none-eabi-gcc -Wall -O0 -mcpu=arm7tdmi-s -marm -c rtos.s -o rtos.o
+arm-none-eabi-gcc -Wall -O0 -mcpu=arm7tdmi-s -marm -c rtos_ll.s -o rtos_ll.o
+arm-none-eabi-gcc -Wall -O0 -mcpu=arm7tdmi-s -marm -c rtos.c -o rtos.o
 arm-none-eabi-gcc -Wall -O0 -mcpu=arm7tdmi-s -marm -c main.c -o main.o
 arm-none-eabi-ld *.o -TLPC2129.ld -o main.elf
 arm-none-eabi-objcopy -O ihex main.elf main.hex
