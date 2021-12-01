6379cece16be363cc5bdf40996e90f608ff3e557
dominik-chat <dominik.sz.chat@gmail.com>
2021-12-01 17:22:59 +0100

    ARM: Stack Pointer swap
    Swapping stack pointer instead of Program Counter.

--- ARM/rtos.s
+++ ARM/rtos.s
@@ -1,13 +1,29 @@
-.global pc_swap
+.global cor_init
 .global cor_start
+.global sp_swap
+
 
 .section  .text
 
-pc_swap:
-	str	lr, [r0]	//Save return address to first argument pointer
-	mov	lr, r1		//Copy second argument to link register
-	bx	lr		//Jump to PC value of second argument
+
+cor_init:
+	mov	r2, sp		//Save old stack
+	mov	sp, r0		//Change to new stack
+	push	{r1}		//Push corutine address to stack
+
+	mov	r0, sp		//Save stack pointer after push
+	mov	sp, r2		//Restore old stack
+	bx	lr		//Return
 
 cor_start:
-	mov	lr, r0		//Copy target function address to link register
+	mov	sp, r0		//Copy target function address to stack pointer
+	pop	{lr}		//Get branch address from stack
 	bx	lr		//Jump to target function
+
+sp_swap:
+	push	{lr}		//Store return address on old stack
+	str	sp, [r0]	//Save old SP to r0 address
+
+	mov	sp, r1		//Set sp to new address
+	pop	{lr}		//Get return address from new stack
+	bx	lr		//Return
