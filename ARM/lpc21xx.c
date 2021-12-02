aa2a48ae0686e0a5237f632e06ea94401a152ac9
dominik-chat <dominik.sz.chat@gmail.com>
2021-12-02 01:17:54 +0100

    ARM: Add ARM preemption
    Preemption uses User Mode for threads, and exception modes for IRQ.

--- ARM/lpc21xx.c
+++ ARM/lpc21xx.c
@@ -1,19 +1,6 @@
 #include "lpc21xx.h"
 
 
-#define PLOCK_bit		(1<<10)
-#define PLLCFG_M(M)		((M-1)&(0x1F))
-#define PLL_MAGIC1		0xAA
-#define PLL_MAGIC2		0x55
-#define PLLE_bit		(1<<0)	//enable
-#define PLLC_bit		(1<<1)	//connect
-
-#define MAM_DISABLE_bm		0x00
-#define MAM_PART_ENABLE_bm	0x01
-#define MAM_ENABLE_bm		0x02
-#define MAM_TIMING_bm(cyc)	(cyc&0x07)
-
-
 _weak _interrupt(UNDEF) void Int_UndefinedInstruction(void)
 {
 	while(1)
@@ -47,27 +34,4 @@ _weak _interrupt(FIQ) void Int_FIQ(void)
 	while(1)
 	{
 	}
-}
-
-void InitPLL(unsigned char ucMultiplier, unsigned char ucDividerMask)
-{
-	PLLCFG = (PLLCFG_M(ucMultiplier) | ucDividerMask);	          
-    PLLFEED = PLL_MAGIC1;					//magic pozwala na zapisanie rejestrow pll
-    PLLFEED = PLL_MAGIC2;
-	
-    PLLCON = PLLE_bit;                		//wlacz pll
-    PLLFEED = PLL_MAGIC1;
-    PLLFEED = PLL_MAGIC2;
-	
-    while(!(PLLSTAT & PLOCK_bit)){}			//czekaj na lock
-	
-    PLLCON = (PLLE_bit | PLLC_bit);			//zamien zegar na pll
-    PLLFEED = PLL_MAGIC1;
-    PLLFEED = PLL_MAGIC2;
-}
-
-void InitMAM(unsigned char ucPrefetchCycles)
-{
-	MAMCR = MAM_ENABLE_bm;						//wlacz prefetch
-    MAMTIM = MAM_TIMING_bm(ucPrefetchCycles);	//ustawia ilosc cycli fetchu
 }
--- ARM/main.c
+++ ARM/main.c
@@ -3,9 +3,21 @@
 
 #include "rtos.h"
 
+
+#define mCOUNTER_ENABLE			1
+#define mINTERRUPT_ON_MR0		1
+#define mRESET_ON_MR0			(1<<1)
+#define mMR0_INTERRUPT			1
+#define VIC_TIMER0_CHANNEL_NR	4
+#define mIRQ_SLOT_ENABLE		(1<<5)
+
+
+extern void timer0_int_handler(void);
+
+
 /* This is a function to simplify proc_* procedures */
 void delay(void){
-	for(unsigned int uiDelayCounter = 0; uiDelayCounter < 500000; uiDelayCounter++){}
+	for(unsigned int uiDelayCounter = 0; uiDelayCounter < 25000; uiDelayCounter++){}
 }
 
 /* This is a function to simplify proc_* procedures */
@@ -26,8 +38,7 @@ void proc_A(void)
 	{
 		pin_toggle(16);
 		delay();
-
-		yield();
+		delay();
 	}
 }
 
@@ -37,20 +48,36 @@ void proc_B(void)
 	{
 		pin_toggle(17);
 		delay();
-
-		yield();
 	}
+}
+
+void timer0_callback(void)
+{
+	resched();
+
+	T0IR = mMR0_INTERRUPT;
+	VICVectAddr = 0x00;
+}
+
+void timer_interrupts_init(unsigned int uiPeriod)
+{
+	VICIntEnable |= (0x1 << VIC_TIMER0_CHANNEL_NR);
+	VICVectCntl0  = mIRQ_SLOT_ENABLE | VIC_TIMER0_CHANNEL_NR;
+	VICVectAddr0  = (unsigned long)timer0_int_handler;
+
+	T0MR0 = 15 * uiPeriod;
+	T0MCR |= (mINTERRUPT_ON_MR0 | mRESET_ON_MR0);
+
+	T0TCR |=  mCOUNTER_ENABLE;
 }
 
 int main()
 {
-	InitPLL(5, PLLCFG_P_2_bm);
-	InitMAM(4);
-
 	pin_init();
 
 	proc_create(proc_A);
 	proc_create(proc_B);
 
+	timer_interrupts_init(1000);
 	proc_run(0);
 }
