bdaa3eaf31549cf54b41ee7964680f2be238ec63
dominik-chat <dominik.sz.chat@gmail.com>
2021-12-18 19:49:10 +0100

    Add ARM semaphores
    Semaphores in supervisor mode

--- ARM/main.c
+++ ARM/main.c
@@ -4,22 +4,6 @@
 #include "rtos.h"
 
 
-#define mCOUNTER_ENABLE			1
-#define mINTERRUPT_ON_MR0		1
-#define mRESET_ON_MR0			(1<<1)
-#define mMR0_INTERRUPT			1
-#define VIC_TIMER0_CHANNEL_NR	4
-#define mIRQ_SLOT_ENABLE		(1<<5)
-
-
-extern void timer0_int_handler(void);
-
-
-/* This is a function to simplify proc_* procedures */
-void delay(void){
-	for(unsigned int uiDelayCounter = 0; uiDelayCounter < 25000; uiDelayCounter++){}
-}
-
 /* This is a function to simplify proc_* procedures */
 void pin_toggle(uint8_t pin_nr){
 	uint32_t mask = (1 << pin_nr);
@@ -31,53 +15,51 @@ void pin_init(void){
 	IO1DIR = 0x30000;
 }
 
+void process_A(void)
+{
+	static uint32_t ctr = 0;
 
-void proc_A(void)
+	while(1)
+	{
+		if (ctr==12500)
+		{
+			sem_give(0);
+		}
+		else if(ctr==25000)
+		{
+			sem_give(1);
+			ctr=0;
+		}
+		ctr++;
+	}
+}
+
+void process_B(void)
 {
 	while(1)
 	{
+		sem_take(0);
 		pin_toggle(16);
-		delay();
-		delay();
 	}
 }
 
-void proc_B(void)
+void process_C(void)
 {
 	while(1)
 	{
+		sem_take(1);
 		pin_toggle(17);
-		delay();
 	}
-}
-
-void timer0_callback(void)
-{
-	resched();
-
-	T0IR = mMR0_INTERRUPT;
-	VICVectAddr = 0x00;
-}
-
-void timer_interrupts_init(unsigned int uiPeriod)
-{
-	VICIntEnable |= (0x1 << VIC_TIMER0_CHANNEL_NR);
-	VICVectCntl0  = mIRQ_SLOT_ENABLE | VIC_TIMER0_CHANNEL_NR;
-	VICVectAddr0  = (unsigned long)timer0_int_handler;
-
-	T0MR0 = 15 * uiPeriod;
-	T0MCR |= (mINTERRUPT_ON_MR0 | mRESET_ON_MR0);
-
-	T0TCR |=  mCOUNTER_ENABLE;
 }
 
 int main()
 {
 	pin_init();
 
-	proc_create(proc_A);
-	proc_create(proc_B);
+	proc_create(process_A);
+	proc_create(process_B);
+	proc_create(process_C);
 
-	timer_interrupts_init(1000);
+	proc_interrupts_init(1000);
 	proc_run(0);
 }
--- ARM/rtos.h
+++ ARM/rtos.h
@@ -7,6 +7,10 @@ void proc_create(void *proc_address);
 
 void proc_run(uint8_t proc_number);
 
-void resched(void);
+void sem_take(uint8_t sem_nr);
+
+void sem_give(uint8_t sem_nr);
+
+void proc_interrupts_init(unsigned int uiPeriod);
 
 #endif /* RTOS_H_ */
